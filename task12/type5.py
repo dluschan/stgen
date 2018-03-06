49d26fecc250c308c6bf4f8778ddedad7467b2cd
Dmitry Luschan <dluschan@gmail.com>
2018-03-06 12:08:03 +0300

    исправлена ошика. вместо количества нулей считалось количество единиц.

--- task12/type5.py
+++ task12/type5.py
@@ -4,15 +4,17 @@ from random import choice
 class Type5(Common):
     def __init__(self):
         self.task = TripleIPAddresses()
-        self.extreme = {max: ["наибольшее количество единиц", "наименьшее количество нулей"], min: ["наибольшее количество нулей", "наименьшее количество единиц"]}
+        self.extreme = {max: ["наибольшее количество единиц", "наименьшее количество нулей"], min: ["наименьшее количество единиц", "наибольшее количество нулей"]}
         self.key = choice(list(self.extreme.keys()))
+        self.fun = [lambda x: x, lambda x: 32 - x]
+        self.digit = choice([0, 1])
 
     def category(self):
         return Common.category(self) + 'Тип 5'
 
     def question_text(self):
         question = "Для узла с IP-адресом {host} адрес сети равен {network}. Определите какое {digits} может быть в маске сети.<br>Ответ запишите в виде десятичного числа."
-        return Common.question_text(self) + question.format(**{'host': self.latex(self.task.host()), 'network': self.latex(self.task.network()), 'digits': choice(self.extreme[self.key])})
+        return Common.question_text(self) + question.format(**{'host': self.latex(self.task.host()), 'network': self.latex(self.task.network()), 'digits': self.extreme[self.key][self.digit]})
 
     def question_answer(self):
-        return self.key(self.task.suitable()).ones()
+        return self.fun[self.digit](self.key(self.task.suitable()).ones())
