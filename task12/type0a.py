494b16a8709c4448681e8862a69058c846f2095c
Dmitry Luschan <dluschan@gmail.com>
2018-03-06 12:15:29 +0300

    испрвлена ошибка. формат ответа и вопроса был не согласован.

--- task12/type0a.py
+++ task12/type0a.py
@@ -8,7 +8,7 @@ class Type0a(Common):
         return Common.category(self) + 'Тип 0a'
 
     def question_text(self):
-        question = "Определите адрес сети, если адрес компьютера в этой сети {host}, а маска сети {netmask}. В качестве ответа укажите адрес сети, записанный по тем же правилам, что и IP-адрес."
+        question = "Определите адрес сети, если адрес компьютера в этой сети {host}, а маска сети {netmask}. В качестве ответа укажите сумму всех байт адреса сети в виде десятичного числа."
         return Common.question_text(self) + question.format(**{'host': self.latex(self.task.host()), 'netmask': self.latex(self.task.netmask())})
 
     def question_answer(self):
--- task12/type0b.py
+++ task12/type0b.py
@@ -8,7 +8,7 @@ class Type0b(Common):
         return Common.category(self) + 'Тип 0b'
 
     def question_text(self):
-        question = "Определите адрес сети, если адрес компьютера в этой сети {ip}, а маска сети {netmask}. В качестве ответа укажите адрес сети в виде 4 байт разделённых точками."
+        question = "Определите адрес сети, если адрес компьютера в этой сети {host}, а маска сети {netmask}. В качестве ответа укажите адрес сети, записанный по тем же правилам, что и IP-адрес."
         return Common.question_text(self) + question.format(**{'ip': self.latex(self.task.host()), 'netmask': self.latex(self.task.netmask())})
 
     def question_answer(self):
