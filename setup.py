c3d2aba23a2a30191cb7ea6ed8279d80b6ed6b4b
Dmitry Luschan <dluschan@gmail.com>
2018-03-14 15:33:20 +0300

    файл для установки пакета

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'use',
+    version = '0.1',
+    packages = find_packages(),
+    author = 'Dmitry Luschan',
+    author_email = 'dluschan@gmail.com',
+    keywords = 'info informatics use exam',
+    description = 'Package for generate questions for informatics USE (moodle format).',
+    install_requires = ['urwid'],
+    entry_points = {
+        'console_scripts': [
+            'exam = main:main',
+        ],
+    },
+)
