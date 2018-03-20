f4dafa3c5fc897f03003201b4212f5f361b5d1e1
Dmitry Luschan <dluschan@gmail.com>
2018-03-20 14:42:55 +0300

    тесты перевода чисел в разные системы счисления

---
+++ tests/notation.py
@@ -0,0 +1,16 @@
+import unittest
+from use.notation import transform
+
+class TestTransform(unittest.TestCase):
+    def test_transform(self):
+        self.assertEqual(transform(255, 2), '11111111')
+
+    def test_transform_abc(self):
+        self.assertEqual(transform(255, 2, 'qw'), 'wwwwwwww')
+
+    def test_transform_based(self):
+        self.assertEqual(transform(255, 2, based = True), '11111111_{2}')
+
+    def test_transform_error(self):
+        with self.assertRaises(IndexError):
+            transform(255, 2, 'q')
