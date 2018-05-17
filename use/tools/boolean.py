b2a60baaa0ced58e8d93f8213141ba2e2fa58159
Dmitry Luschan <dluschan@gmail.com>
2018-05-17 03:18:24 +0300

    переработана поддержка булевой логики

--- use/tools/boolean.py
+++ use/tools/boolean.py
@@ -1,49 +1,226 @@
 class Term:
-    def __init__(self, name):
-        assert(type(name) == str)
-        self.name = name
+	def braced(self, priority):
+		return '\\left(' + str(self) + '\\right)' if self.priority < priority else '{' + str(self) + '}'
 
-    def __str__(self):
-        return self.name
 
-    def __call__(self, **kwargs):
-        return kwargs[self.name]
+class Brackets(Term):
+	priority = 4
 
-class UnaryOperation:
-    repr = 'unary'
+	def __init__(self, *args):
+		assert(len(args) == 1)
+		self.args = args
 
-    operation = lambda self, a: 0
+	def __str__(self):
+		return '(' + str(self.args[0]) + ')'
 
-    def __init__(self, *args):
-        assert(len(args) == 1)
-        self.args = args
+	def __repr__(self):
+		return '\\left(' + repr(self.args[0]) + '\\right)'
 
-    def __str__(self):
-        return self.repr + ' { ' + str(self.args[0]) + ' }'
+	@staticmethod
+	def __call__(self, **kwargs):
+		return self.args[0](**kwargs)
 
-    def __call__(self, **kwargs):
-        return self.operation(self.args[0](**kwargs))
 
-Positive = type('Positive', (UnaryOperation,), {'repr': '', 'operation': lambda self, a: a})
-Negation = type('Negation', (UnaryOperation,), {'repr': '\\overline', 'operation': lambda self, a: not a})
+class Variable(Term):
+	priority = 5
 
-class BinaryOperation:
-    repr = 'binary'
+	def __init__(self, name):
+		assert(type(name) == str)
+		self.family = name[0]
+		self.order = int(name[1:])
 
-    operation = lambda self, a, b: 0
+	def __repr__(self):
+		return self.family + '_{' + str(self.order) + '}'
 
-    def __init__(self, *args):
-        assert(len(args) == 2)
-        self.args = args
+	def __str__(self):
+		return self.family + str(self.order)
 
-    def __str__(self):
-        return '(' + '{ ' + str(self.args[0]) + ' } ' + self.repr + ' { ' + str(self.args[1]) + ' }' + ')'
+	def __call__(self, **kwargs):
+		return kwargs[str(self)]
 
-    def __call__(self, **kwargs):
-        return self.operation(self.args[0](**kwargs), self.args[1](**kwargs))
+	def __hash__(self):
+		return hash(str(self))
 
-Conjunction = type('Conjunction', (BinaryOperation,), {'repr': '\\wedge', 'operation': lambda self, a, b: a and b})
-Disjunction = type('Disjunction', (BinaryOperation,), {'repr': '\\vee', 'operation': lambda self, a, b: a or b})
-Implication = type('Implication', (BinaryOperation,), {'repr': '\\implies', 'operation': lambda self, a, b: not a or b})
-Equal = type('Equal', (BinaryOperation,), {'repr': '\\equiv', 'operation': lambda self, a, b: a == b})
-Notequal = type('Notequal', (BinaryOperation,), {'repr': '\\neq', 'operation': lambda self, a, b: a != b})
+	def __eq__(self, other):
+		return self.family == other.family and self.order == other.order
+
+
+class Constant(Term):
+	priority = 5
+
+	def __init__(self):
+		pass
+
+
+class TrueConst(Constant):
+	@staticmethod
+	def __repr__():
+		return '1'
+
+	@staticmethod
+	def __call__(**kwargs):
+		return True
+
+
+class FalseConst(Constant):
+	@staticmethod
+	def __repr__():
+		return '0'
+
+	@staticmethod
+	def __call__(**kwargs):
+		return False
+
+
+class LogicOperation(Term):
+	pass
+
+
+class UnaryLogicOperation(LogicOperation):
+	priority = 4
+
+	def __init__(self, *args):
+		assert(len(args) == 1)
+		self.args = args
+
+	def __repr__(self):
+		return self.repr + self.args[0].braced(self.priority)
+
+	def __call__(self, **kwargs):
+		return self.operation(self.args[0](**kwargs))
+
+
+class Positive(UnaryLogicOperation):
+	repr = ''
+
+	@staticmethod
+	def operation(a):
+		return a
+
+
+class Negation(UnaryLogicOperation):
+	repr = '\\overline'
+
+	@staticmethod
+	def operation(a):
+		return not a
+
+
+class BinaryLogicOperation(LogicOperation):
+	priority = 0
+
+	def __init__(self, *args):
+		assert(len(args) == 2)
+		self.args = args
+
+	def __repr__(self):
+		return self.args[0].braced(self.priority) + self.repr + self.args[1].braced(self.priority)
+
+	def __str__(self):
+		return self.args[0].braced(self.priority) + self.symbol + self.args[1].braced(self.priority)
+
+	def __call__(self, **kwargs):
+		return self.operation(self.args[0](**kwargs), self.args[1](**kwargs))
+
+
+class Conjunction(BinaryLogicOperation):
+	priority = 3
+	repr = '\\wedge'
+
+	@staticmethod
+	def operation(a, b):
+		return a and b
+
+
+class Disjunction(BinaryLogicOperation):
+	priority = 2
+	repr = '\\vee'
+
+	@staticmethod
+	def operation(a, b):
+		return a or b
+
+
+class Implication(BinaryLogicOperation):
+	priority = 1
+	repr = '\\implies'
+
+	@staticmethod
+	def operation(a, b):
+		return not a or b
+
+
+class Equal(BinaryLogicOperation):
+	priority = 0
+	repr = '\\equiv'
+
+	@staticmethod
+	def operation(a, b):
+		return a == b
+
+
+class Notequal(BinaryLogicOperation):
+	priority = 0
+	repr = '\\neq'
+
+	@staticmethod
+	def operation(a, b):
+		return a != b
+
+
+def analyze(t):
+	r = {}
+	ranalyze(t, r)
+	return r
+
+
+def ranalyze(t, r):
+	if type(t) in [FalseConst, TrueConst]:
+		pass
+	elif type(t) == Variable:
+		if t.family not in r:
+			r[t.family] = [t.order]
+		elif t.order not in r[t.family]:
+			r[t.family] += [t.order]
+	elif type(t) in UnaryLogicOperation.__subclasses__() + BinaryLogicOperation.__subclasses__():
+		for arg in t.args:
+			ranalyze(arg, r)
+
+
+def shift(t, k):
+	import copy
+	c = copy.deepcopy(t)
+	rshift(c, k)
+	return c
+
+
+def rshift(t, k):
+	if type(t) in [FalseConst, TrueConst]:
+		pass
+	elif type(t) == Variable:
+		t.order += k if type(k) == int else k[t.family]
+	elif type(t) in UnaryLogicOperation.__subclasses__() + BinaryLogicOperation.__subclasses__():
+		for arg in t.args:
+			rshift(arg, k)
+
+
+def terms(t):
+	r = []
+	rterms(t, r)
+	return r
+
+
+def rterms(t, r):
+	if type(t) in [FalseConst, TrueConst]:
+		pass
+	elif type(t) == Variable and t not in r:
+		r.append(t)
+	elif type(t) in UnaryLogicOperation.__subclasses__() + BinaryLogicOperation.__subclasses__():
+		for arg in t.args:
+			rterms(arg, r)
+
+
+def varlist(t):
+	z = analyze(t)
+	return [key + str(order) for key in z for order in z[key]]
+
