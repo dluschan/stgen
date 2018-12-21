79689ae13fd9f55803a7961381b062418e4fa7af
Dmitry Luschan <dluschan@gmail.com>
2018-12-21 23:57:53 +0300

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
 	name='stgen',
-	version='0.2.0',
+	version='0.2.1',
 	author='Dmitry Luschan',
 	author_email = 'dluschan@gmail.com',
 	description='School tasks generator',
