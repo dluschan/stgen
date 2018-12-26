b9911dbc18670154c36e16176cadbc6675882d29
Dmitry Luschan <dluschan@gmail.com>
2018-12-26 03:51:10 +0300

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
 	name='stgen',
-	version='0.3.0',
+	version='0.3.1',
 	author='Dmitry Luschan',
 	author_email='dluschan@gmail.com',
 	description='School tasks generator',
