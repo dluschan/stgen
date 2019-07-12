89369d7a9e21264b8a575aedb23388157ba459ea
Dmitry Luschan <dluschan@gmail.com>
2019-07-12 18:41:52 +0300

    Тестирование генерации входных данных для задач на программирование

--- tests/program_io.py
+++ tests/program_io.py
@@ -3,7 +3,7 @@ from stgen.tools.program_io import *
 
 
 class TestIOFormat(unittest.TestCase):
-	"""Тестирование генерации входных и выходных данных для задач на программирование."""
+	"""Тестирование генерации описания входных и выходных данных для задач на программирование."""
 	def test_input_0(self):
 		params = {'n': [int, [1, 10 ** 4], 'количество чисел']}
 		input_format = """n"""
@@ -34,3 +34,40 @@ x*n"""
 		output_format = """s|k"""
 		self.assertEqual(parse_output(output_format, params), """Формат выходных данных
 В \\( k \\) строках выводятся высоты \\( s \\).""")
+
+
+class TestInputGenerate(unittest.TestCase):
+	"""Тестирование генерации входных данных для задач на программирование."""
+	def test_0(self):
+		params = {'n': [int, [1, 10 ** 4], 'количество чисел']}
+		input_format = """n"""
+		s = generate_input(input_format, params)
+		self.assertTrue(params[input_format][1][0] <= params[input_format][0](s) <= params[input_format][1][1])
+
+	def test_1(self):
+		params = {
+			'n': [int, [1, 10], 'количество чисел'],
+			'x': [float, [1, 10**3], 'число'],
+		}
+		input_format = """n
+x|n"""
+		s = generate_input(input_format, params)
+		head, *tail, empty = s.split("\n")
+		self.assertEqual(params["n"][0](head), len(tail))
+		for num in tail:
+			self.assertTrue(params["x"][1][0] <= params["x"][0](num) <= params["x"][1][1])
+
+	def test_2(self):
+		params = {
+			'n': [int, [1, 10], 'количество чисел'],
+			'x': [float, [1, 10**3], 'число'],
+		}
+		input_format = """n
+x*n"""
+		s = generate_input(input_format, params)
+		head, tail, empty = s.split("\n")
+		tail = tail.split()
+		self.assertEqual(params["n"][0](head), len(tail))
+		for num in tail:
+			self.assertTrue(params["x"][1][0] <= params["x"][0](num) <= params["x"][1][1])
+
