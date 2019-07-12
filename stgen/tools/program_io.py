35bda0a930b3998dc43375ca24365b23581eff05
Dmitry Luschan <dluschan@gmail.com>
2019-07-12 18:40:07 +0300

    добавлены функции генерации входных данных по описанию

--- stgen/tools/program_io.py
+++ stgen/tools/program_io.py
@@ -1,4 +1,5 @@
 from .common import latex
+from random import randint, uniform, expovariate
 
 
 def parse_line(s, params):
@@ -49,3 +50,34 @@ def parse_output(s, params):
 			res += begin + end + parse_line(s[0], params)
 	return res.strip()
 
+
+def generate_item(x, params, generated):
+	if params[x][0] == int:
+		magic_const = 10
+		generated[x] = max(min(params[x][1][1], int(expovariate(magic_const / params[x][1][1]))), params[x][1][0])
+	elif params[x][0] == float:
+		generated[x] = uniform(*params[x][1])
+	elif params[x][0] == str:
+		generated[x] = ""
+	return generated[x]
+
+
+def generate_line(s, params, generated):
+	q = s.split("*")
+	if len(q) > 1:
+		return " ".join([str(generate_item(q[0], params, generated)) for _ in range(eval(q[1], generated))])
+	else:
+		return " ".join(str(generate_item(x, params, generated)) for x in q[0].split())
+
+
+def generate_input(s, params):
+	generated = {}
+	res = ""
+	for line in s.split("\n"):
+		parts = line.split("|")
+		if len(parts) > 1:
+			for i in range(eval(parts[1], generated)):
+				res += generate_line(parts[0], params, generated) + "\n"
+		else:
+			res += generate_line(parts[0], params, generated) + "\n"
+	return res
