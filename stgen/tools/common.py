92088d13fff85e95b4e654464f46440b4593f0b7
Dmitry Luschan <dluschan@gmail.com>
2019-09-20 19:06:06 +0300

    добавлен генератор простых чисел

--- stgen/tools/common.py
+++ stgen/tools/common.py
@@ -3,3 +3,17 @@ letters = 'abcdefghijklmnopqrstuvwxyz'
 
 def latex(s):
 	return "\\( " + s + " \\)"
+
+
+def get_primes(n):
+	"""возвращает список из первых n простых чисел"""
+	assert n > 0, "количество простых чисел должно быть больше нуля"
+	primes = [2]
+
+	for _ in range(n - 1):
+		next = primes[-1] + 1
+		while any(next % x == 0 for x in primes):
+			next += 1
+		primes.append(next)
+
+	return primes
