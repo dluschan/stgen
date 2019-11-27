11c811b7adf4d5a970f61a6be620d490bc522ffb
Dmitry Luschan <dluschan@gmail.com>
2019-11-27 18:38:33 +0300

    Пакетный режим работы может запускать нужные генераторы

--- stgen/main.py
+++ stgen/main.py
@@ -1,27 +1,31 @@
-"""
-usage: stgen batch -t 7 12 -c 1500 -o output.xml
-usage: stgen tui
-"""
 import argparse
 from .tui import tui
 from .tools.generator import MainGenerator
+from .tools.task import BaseTask, CodeRunnerTask
+
+
+def all_subclasses(cls):
+    return cls.__subclasses__() + [g for s in cls.__subclasses__() for g in all_subclasses(s)]
 
 
 def createParser():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='command')
     batch_parser = subparsers.add_parser('batch')
-    batch_parser.add_argument('-t', nargs='+', type=int)
-    batch_parser.add_argument('-c', type=int)
-    batch_parser.add_argument('-o', type=argparse.FileType(mode='w', bufsize=-1, encoding=None, errors=None))
+    batch_parser.add_argument('--task', '-t', nargs='+', type=int, required=True)
+    batch_parser.add_argument('--count', '-c', type=int, required=True)
+    batch_parser.add_argument('--output', '-o', type=argparse.FileType(mode='w', bufsize=-1, encoding=None, errors=None), required=True)
     interactive_parser = subparsers.add_parser('tui')
     return parser
 
 
 def batch(namespace):
-    return 0
-    choiced = ['Task']
-    print(MainGenerator(choiced).generate(10), file=open(output.get_edit_text(), 'w'))
+    choiced = []
+    clses = {cls.__name__.split('.')[1] if cls.__name__.count('.') else cls.__name__: cls for cls in BaseTask.__subclasses__() + CodeRunnerTask.__subclasses__()}
+    for t in namespace.task:
+        class_name = 'Task' + str(t).zfill(2)
+        choiced += [g for g in all_subclasses(clses[class_name]) if g.__subclasses__() == []]
+    print(MainGenerator(choiced).generate(namespace.count), file=namespace.output)
 
 
 def main():
