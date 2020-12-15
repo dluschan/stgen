15d05df49a793e031957ebc7615c2bc77556cabe
Dmitry Luschan <dluschan@gmail.com>
2020-12-15 14:48:14 +0300

    вспомогательные функции для построения графа оперируют сразу списками узлов вместо их индексов

--- stgen/task15/common.py
+++ stgen/task15/common.py
@@ -1,4 +1,4 @@
-from random import randint, choice, shuffle
+from random import choice, randint, sample, shuffle
 from pydot import graph_from_dot_data
 from pygraph.classes.digraph import digraph
 from pygraph.readwrite import dot
@@ -8,13 +8,8 @@ from ..tools.task import BaseTask
 def horizontal(x):
 	""""Функция устанавливает горизонтальные (т.е. между вершинами одного слоя) связи в графе"""
 	k = randint(1, (len(x) + 1) // 2)
-	hor = set()
-	while len(hor) < k:
-		hor.add(choice(x[:-1]))
-	res = []
-	for w in hor:
-		res.append((w, w+1))
-	return res
+	for i in sample(range(len(x) - 1), k):
+		yield (x[i], x[i + 1])
 
 
 def distribution(a, b):
@@ -49,19 +44,17 @@ def addition(x, y):
 
 
 def graph(nodes):
-	""""Функция генерирует граф заданной длины из набора вершин в виде списка пар вершин"""
-	n = len(nodes)
-	k = randint(3, (n + 2) // 3)
-	levels = [[0], *distribution(list(range(1, n - 1)), k), [n - 1]]
-	res = []
-	for i in range(len(levels) - 1):
-		if len(levels[i]) >= 2:
-			res += horizontal(levels[i][:])
-		res += addition(levels[i][:], levels[i+1][:])
+	""""Функция генерирует направленный граф из списка вершин"""
 	g = digraph()
 	g.add_nodes(nodes)
-	for begin, end in res:
-		g.add_edge((nodes[begin], nodes[end]))
+	k = randint(3, (len(nodes) + 2) // 3)
+	levels = [nodes[:1], *distribution(nodes[1:-1], k), nodes[-1:]]
+	for i in range(len(levels) - 1):
+		if len(levels[i]) >= 2:
+			for edge in horizontal(levels[i]):
+				g.add_edge(edge)
+		for edge in addition(levels[i][:], levels[i+1][:]):
+			g.add_edge(edge)
 	return g
 
 
@@ -91,7 +84,7 @@ class Task15(BaseTask):
 	def __init__(self):
 		self.n = randint(10, 18)
 		self.nodes = 'ДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'[:self.n]
-		self.g = graph(self.nodes)
+		self.g = graph(list(self.nodes))
 		self.question = """На рисунке - схема дорог, связывающих города {cites}.
 			По каждой дороге можно двигаться только в одном направлении, указанном стрелкой.
 			Сколько существует различных путей из города {begin} в город {end}? {graph}"""
