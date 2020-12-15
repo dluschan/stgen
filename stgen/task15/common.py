61eab6f8f2bb8abc90ae9f6d51faeee73b84b15c
Dmitry Luschan <dluschan@gmail.com>
2020-12-15 12:52:23 +0300

    граф для описания схемы дорог хранится в виде структуры pygraph.classes.digraph.digraph

--- stgen/task15/common.py
+++ stgen/task15/common.py
@@ -1,5 +1,7 @@
 from random import randint, choice, shuffle
-import pydot
+from pydot import graph_from_dot_data
+from pygraph.classes.digraph import digraph
+from pygraph.readwrite import dot
 from ..tools.task import BaseTask
 
 
@@ -46,8 +48,9 @@ def addition(x, y):
 	return list(zip(a, b))
 
 
-def graph(n):
+def graph(nodes):
 	""""Функция генерирует граф заданной длины из набора вершин в виде списка пар вершин"""
+	n = len(nodes)
 	k = randint(3, (n + 2) // 3)
 	levels = [[0], *distribution(list(range(1, n - 1)), k), [n - 1]]
 	res = []
@@ -55,46 +58,40 @@ def graph(n):
 		if len(levels[i]) >= 2:
 			res += horizontal(levels[i][:])
 		res += addition(levels[i][:], levels[i+1][:])
-	return res
+	g = digraph()
+	g.add_nodes(nodes)
+	for begin, end in res:
+		g.add_edge((nodes[begin], nodes[end]))
+	return g
 
 
-def dot_graph(g, s):
-	"""Конвертирует граф в формат dot с переименованием вершин согласно строке s"""
-	graph = pydot.Dot(graph_type='digraph', rankdir='LR')
-	node = pydot.Node('node')
-	node.set("shape", 'circle')
-	graph.add_node(node)
-	for a, b in g:
-		graph.add_edge(pydot.Edge(s[a], s[b]))
-	return graph
+def dot_graph(g):
+	"""Конвертирует граф в формат dot"""
+	graph_dot = graph_from_dot_data(dot.write(g))[0]
+	graph_dot.set_rankdir('LR')
+	for node in graph_dot.get_nodes():
+		node.set_shape('circle')
+	return graph_dot
 
 
-def dict_graph(links):
-	"""Функция преобразовывает граф из списка связей в список списков"""
-	res = {}
-	for key in set([x[0] for x in links] + [x[1] for x in links]):
-		res[key] = [y[0] for y in filter(lambda x: x[1] == key, links)]
-	return res
-
-
-def solve(links, start, end):
+def solve(graph, start, end):
 	"""Функция подсчитывает количество путей в графе"""
 	if start == end:
 		return 1
 	else:
-		return sum([solve(links, start, finish) for finish in links[end]])
+		return sum([solve(graph, start, finish) for finish in graph.incidents(end)])
 
 
-def dot(g, s):
-	return dot_graph(g, s).create(format='svg').decode('utf-8')
+def dot_to_svg(g):
+	return dot_graph(g).create(format='svg').decode('utf-8')
 
 
 class Task15(BaseTask):
 	"""Поиск количества путей на графе."""
 	def __init__(self):
-		self.s = 'АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 		self.n = randint(10, 18)
-		self.g = graph(self.n)
+		self.nodes = 'ДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'[:self.n]
+		self.g = graph(self.nodes)
 		self.question = """На рисунке - схема дорог, связывающих города {cites}.
 			По каждой дороге можно двигаться только в одном направлении, указанном стрелкой.
 			Сколько существует различных путей из города {begin} в город {end}? {graph}"""
@@ -103,10 +100,10 @@ class Task15(BaseTask):
 		return super().category() + 'Задача 15/'
 
 	def question_text(self):
-		return self.question.format(cites=', '.join(self.s[:self.n]), begin=self.s[0], end=self.s[self.n-1], graph=dot(self.g, self.s))
+		return self.question.format(cites=', '.join(self.nodes), begin=self.nodes[0], end=self.nodes[-1], graph=dot_to_svg(self.g))
 
 	def question_answer(self):
-		return solve(dict_graph(self.g), 0, self.n - 1)
+		return solve(self.g, self.nodes[0], self.nodes[-1])
 
 	def question_type(self):
 		return 'numerical'
